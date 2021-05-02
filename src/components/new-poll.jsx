36fafe5a5a7846e3dfc106d3f4f378526a8bece4
sarahelhawary <sarah.elhwarii92@gmail.com>
2021-05-02 19:25:54 +0200

    modify new-poll component

--- src/components/new-poll.jsx
+++ src/components/new-poll.jsx
@@ -1,18 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import classes from './new-poll.module.css'
+import { formatQuestion } from '../utils/API'
+import { useDispatch } from 'react-redux'
+import { addQuestion } from '../slices/questions'
 
-export default function NewPoll() {
-  const [value, setValue] = useState('hey')
-  // const users = useSelector((state) => state.users)
-  // useEffect(() => {
-  //   console.log(users, value)
-  // })
+export default function NewPoll(props) {
+  const questions = useSelector((state) => state.questions.value)
 
-  // function handleSubmit(e) {
-  //   e.preventDefualt()
-  // }
+  const [optionOne, setOptionOne] = useState('')
+  const [optionTwo, setOptionTwo] = useState('')
+
+  const dispatch = useDispatch()
+
+  //change handlers for inputs(handleOptionOneChange,...)
+
+  const handleOptionOneChange = (event) => setOptionOne(event.target.value)
+  const handleOptionTwoChange = (event) => setOptionTwo(event.target.value)
+
+  //handle form submit(handleSubmit)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    //formatQuestion
+
+    const question = formatQuestion({
+      optionOneText: optionOne,
+      optionTwoText: optionTwo,
+      author: 'sarahedo',
+    })
+    //dispatch(addQuestion)
+    dispatch(addQuestion(question))
+    // console.log('submitted')
+    console.log(questions)
+  }
+  const isDisabled = () => !optionOne || !optionTwo
+
   return (
     <div className={classes.newPoll}>
       <div className={classes.head}>
@@ -21,19 +44,25 @@ export default function NewPoll() {
       <div className={classes.newPollInfo}>
         <p>Complete the question: </p>
         <h4 className={classes.subtitle}>Would you rather...</h4>
-        <form className={classes.form}>
+        <form onSubmit={handleSubmit} className={classes.form}>
           <input
             type="text"
             className={classes.input}
             placeholder="Enter option one..."
+            onChange={handleOptionOneChange}
+            value={optionOne}
           />
           <h4>or...</h4>
           <input
             type="text"
             className={classes.input}
             placeholder="Enter option two..."
+            onChange={handleOptionTwoChange}
+            value={optionTwo}
           />
-          <button className={classes.btn}>Submit</button>
+          <button className={classes.btn} disabled={isDisabled()}>
+            Submit
+          </button>
         </form>
       </div>
     </div>
