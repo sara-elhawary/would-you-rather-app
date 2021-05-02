0d43132de677ed89a49963dede2f444815f09a6f
sarahelhawary <sarah.elhwarii92@gmail.com>
2021-05-02 08:43:13 +0200

    get state using useSelector hook

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 import Poll from '../components/poll'
 import classes from './home.module.css'
 
 export default function Home() {
-  const users = useSelector((state) => state)
-  // const dispatch = useDispatch()
-
-  // console.log(users)
+  //users cuz of reducer name
+  const users = Object.values(useSelector((state) => state.users.value))
+  const questions = useSelector((state) => state.questions.value)
+  console.log(questions)
+  console.log(users)
 
   return (
     <div className={classes.container}>
@@ -17,8 +17,11 @@ export default function Home() {
         <button className={classes.unansweredBtn}>Unanswered</button>
         <button className={classes.answeredBtn}>Answered</button>
       </div>
+      {/* {JSON.stringify(users, null, 2)} */}
       <div className={classes.polls}></div>
-      <Poll />
+      {users.map((user) => (
+        <Poll name={user.name} key={user.id} id={user.id} />
+      ))}
     </div>
   )
 }
