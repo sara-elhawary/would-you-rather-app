7442e84c03f74bc7f91841b21447401e894c687b
sarahelhawary <sarah.elhwarii92@gmail.com>
2021-05-03 06:15:20 +0200

    modify poll answer component

--- src/components/poll-answer.jsx
+++ src/components/poll-answer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 import classes from './poll-answer.module.css'
 
-export default function PollAnswer({ optionOne, optionTwo }) {
+export default function PollAnswer() {
+  const optionOne = 'op 1'
+  const optionTwo = 'op 2'
+  const { id } = useParams()
+
+  const questions = useSelector((state) => state.questions.value)
+
   return (
     <div>
       <h4>Would you rather</h4>
+      {JSON.stringify(questions[id])}
       <form className={classes.form}>
         <div className={classes.formControl}>
           <input type="radio" />
